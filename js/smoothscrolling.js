203b78bf428e2e99e48d0d66a3b1411ea7166cd7
iamvinayp <vinaypatil057@gmail.com>
2016-11-25 13:42:46 +0530

    Added the required portfolio files

---
+++ js/smoothscrolling.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  // Add smooth scrolling to links
+  $("ul a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
